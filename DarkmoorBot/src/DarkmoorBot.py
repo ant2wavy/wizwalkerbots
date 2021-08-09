99c1c757159252563b9cb67e6860c48a57bc5b57
ant2wavy <88689631+ant2wavy@users.noreply.github.com>
2021-08-09 19:43:27 -0400

    Added source for Darkmoor Bot

---
+++ DarkmoorBot/src/DarkmoorBot.py
@@ -0,0 +1,223 @@
+# Credit to MajorPain1 for the core functionality/utils that this bot utilizes.
+
+import asyncio
+from time import time
+
+from wizwalker.constants import Keycode
+from wizwalker.extensions.wizsprinter import SprintyCombat, CombatConfigProvider, WizSprinter
+from wizwalker.extensions.scripting.utils import teleport_to_friend_from_list
+from utils import decide_heal, go_through_dialog, exit_out, go_to_closest_mob
+
+
+async def main(sprinter):
+
+    # Register clients
+    sprinter.get_new_clients()
+    clients = sprinter.get_ordered_clients()
+    p1, p2, p3, p4 = [*clients, None, None, None, None][:4]
+    for i, p in enumerate(clients, 1):
+        p.title = "p" + str(i)
+
+    # Hook activation
+    for p in clients:
+        print(f"[{p.title}] Activating Hooks")
+        await p.activate_hooks()
+        await p.mouse_handler.activate_mouseless()
+        await p.send_key(Keycode.PAGE_DOWN, 0.1)
+        await asyncio.sleep(0.5)
+
+    print()
+
+    Total_Count = 0
+    total = time()
+    while True:
+
+        # Buy potions if needed
+        await asyncio.gather(*[decide_heal(p) for p in clients])
+
+        start = time()
+
+        # Entering Dungeon
+        await asyncio.gather(*[p.send_key(Keycode.X, 0.1) for p in clients])
+        await asyncio.sleep(11)
+        await asyncio.gather(*[p.wait_for_zone_change() for p in clients])
+
+        # Going through first section of dialogue
+        await asyncio.sleep(1)
+        await p1.send_key(Keycode.W, 3)
+        await asyncio.sleep(1)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(1)
+
+        # Moving to first boss fight (Yevgeny)
+        for p in clients:
+            await asyncio.gather(*[go_to_closest_mob(p)])
+
+        # Yevgeny fight
+        print("Starting Yevgeny fight.")
+        combat_handlers = []
+        await asyncio.sleep(2)
+        for p in clients:
+            combat_handlers.append(
+                SprintyCombat(p, CombatConfigProvider(f'sideboss_configs/{p.title}spellconfig.txt', cast_time=2)))
+        await asyncio.gather(*[h.wait_for_combat() for h in combat_handlers])
+        print("Yevgeny defeated, moving to Shane von Shane.")
+        print()
+
+        # Go through second section of dialogue after Yevgeny
+        await asyncio.sleep(1)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(1)
+
+        # Unghosting
+        for p in clients[1:]:
+            await p.send_key(Keycode.W, 0.1)
+            await asyncio.sleep(0.2)
+
+        # Moving through door to Shane Von Shane
+
+        await p1.goto(15.308456420898438, 2946.2978515621)
+        await p1.wait_for_zone_change()
+        await asyncio.sleep(1)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(1)
+
+        for p in clients[1:]:
+            await p.goto(15.308456420898438, 2946.2978515621)
+            await p.wait_for_zone_change()
+            await p.send_key(Keycode.S, 0.1)
+
+        # Moving to make Shane Von Shane appear
+
+        await p1.goto(116.53296661376953, 1298.63916015625)
+        await asyncio.sleep(1)
+        # Go through dialogue triggered by Shane appearance
+        await asyncio.sleep(1)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(1)
+
+        # Moving to second boss fight (Shane von Shane)
+        for p in clients:
+            await asyncio.gather(*[go_to_closest_mob(p)])
+
+        # Second boss fight (Shane Von Shane):
+        print("Starting Shane Von Shane fight.")
+        combat_handlers = []
+        await asyncio.sleep(2)
+        for p in clients:
+            combat_handlers.append(
+                SprintyCombat(p, CombatConfigProvider(f'sideboss_configs/{p.title}spellconfig.txt', cast_time=1.5)))
+        await asyncio.gather(*[h.wait_for_combat() for h in combat_handlers])
+        print("Shane von Shane defeated, moving to Malistaire fight.")
+        print()
+
+        await asyncio.sleep(1)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(1.5)
+
+        # Moving to Mali fight room
+
+        await p1.goto(4.960016250610352, 2900.962158203125)
+        await p1.wait_for_zone_change()
+        await p1.send_key(Keycode.W, 2)
+        await asyncio.sleep(1)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(1)
+
+        for p in clients[1:]:
+            await p.goto(-467.0201416015625, 369.74102783203125)
+            await p.goto(4.960016250610352, 2900.962158203125)
+            await p.wait_for_zone_change()
+            await asyncio.sleep(1)
+
+        # Moving to Bone Dragon Animation
+        await p1.goto(116.53296661376953, 1298.63916015625)
+        await asyncio.sleep(20)
+        await asyncio.gather(*[p.use_potion_if_needed(health_percent=80, mana_percent=10) for p in clients])
+        await asyncio.sleep(1)
+
+        # Engaging mali fight
+        for p in clients:
+            await asyncio.gather(*[go_to_closest_mob(p)])
+
+        # Final boss fight (Malistaire)
+        print("Starting Malistaire fight.")
+        combat_handlers = []
+        await asyncio.sleep(2)
+        for p in clients:
+            combat_handlers.append(
+                SprintyCombat(p, CombatConfigProvider(f'mainboss_configs/{p.title}spellconfig.txt', cast_time=1.5)))
+        await asyncio.gather(*[h.wait_for_combat() for h in combat_handlers])
+        print("Combat ended")
+        print()
+
+        # Clear mali dialogue (post-fight)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(5)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(5)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(5)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(5)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(5)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(5)
+        await asyncio.gather(*[go_through_dialog(p) for p in clients])
+        await asyncio.sleep(60)
+        print("Returning to sigil for next run.")
+        print()
+
+        # Return to p1 to sigil
+        await asyncio.gather(*[exit_out(p1)])
+        await asyncio.sleep(1)
+
+        # p2-p4 port to p1 after conclusion of run (fish icon)
+        for p in clients[1:]:
+            await p.send_key(Keycode.F, 0.1)
+            await teleport_to_friend_from_list(client=p, icon_list=2, icon_index=0, name=None)
+            await p.wait_for_zone_change()
+            await asyncio.sleep(1)
+            await p.send_key(Keycode.PAGE_DOWN, 0.1)
+
+        await asyncio.sleep(2)
+
+        # Healing
+
+        await p1.use_potion_if_needed(health_percent=70, mana_percent=33)
+
+        for p in clients[1:]:
+            await p.use_potion_if_needed(health_percent=70, mana_percent=10)
+            await asyncio.sleep(0.2)
+
+        await asyncio.sleep(3.5)
+        await p1.send_key(Keycode.PAGE_DOWN, 0.1)
+        await asyncio.sleep(1)
+
+        # Time
+        Total_Count += 1
+        print("The Total Amount of Runs: ", Total_Count)
+        print("Time Taken for run: ", round((time() - start) / 60, 2), "minutes")
+        print("Total time elapsed: ", round((time() - total) / 60, 2), "minutes")
+        print("Average time per run: ", round(((time() - total) / 60) / Total_Count, 2), "minutes")
+        print()
+
+
+# Error Handling
+async def run():
+    sprinter = WizSprinter()
+
+    try:
+        await main(sprinter)
+    except:
+        import traceback
+
+        traceback.print_exc()
+
+    await sprinter.close()
+
+
+# Start
+if __name__ == "__main__":
+    asyncio.run(run())
---
+++ DarkmoorBot/src/utils.py
@@ -0,0 +1,147 @@
+# Credit to MajorPain1 for the core functionality/utils that this bot utilizes.
+
+import asyncio
+
+
+from wizwalker import XYZ
+from wizwalker.constants import Keycode
+from wizwalker.extensions.wizsprinter.sprinty_client import MemoryReadError, List, Set
+from wizwalker.memory import DynamicClientObject
+
+potion_ui_buy = [
+    "fillallpotions",
+    "buyAction",
+    "btnShopPotions",
+    "centerButton",
+    "fillonepotion",
+    "buyAction",
+    "exit"
+]
+
+
+async def go_to_closest_mob(self, excluded_ids: Set[int] = None) -> bool:
+    return await go_to_closest_of(self, await self.get_mobs(excluded_ids), False)
+
+
+async def go_to_closest_of(self, entities: List[DynamicClientObject], only_safe: bool = False):
+    if e := await self.find_closest_of_entities(entities, only_safe):
+        ev = await e.location()
+        await self.goto(ev.x, ev.y)
+        return True
+    return False
+
+
+async def exit_out(client):
+    await client.goto(-6.31235408782959, -3578.52978515620)
+    await client.wait_for_zone_change()
+    await asyncio.sleep(1)
+    await client.goto(-467.0201416015625, 369.74102783203125)
+    await client.goto(-8.79665470123291, -2300.393310546875)
+    await client.wait_for_zone_change()
+    await asyncio.sleep(1)
+    await client.goto(-66.913818359375, -3287.22607421875)
+    await asyncio.sleep(0.5)
+    await client.wait_for_zone_change()
+
+
+async def auto_buy_potions(client):
+    # Head to home world gate
+    await asyncio.sleep(0.5)
+    await client.send_key(Keycode.HOME, 0.1)
+    await client.wait_for_zone_change()
+    while not await client.is_in_npc_range():
+        await client.send_key(Keycode.S, 0.1)
+    await client.send_key(Keycode.X, 0.1)
+    await asyncio.sleep(2)
+    # Go to Wizard City
+    await client.mouse_handler.click_window_with_name('wbtnWizardCity')
+    await asyncio.sleep(3)
+    await client.mouse_handler.click_window_with_name('teleportButton')
+    await client.wait_for_zone_change()
+    # Walk to potion vendor
+    await client.goto(-0.5264079570770264, -3021.25244140625)
+    await client.send_key(Keycode.W, 0.5)
+    await client.wait_for_zone_change()
+    await client.goto(11.836355209350586, -1816.455078125)
+    await client.send_key(Keycode.W, 0.5)
+    await client.wait_for_zone_change()
+    await client.goto(-880.2447509765625, 747.2051391601562)
+    await client.goto(-4272.06884765625, 1251.950927734375)
+    await asyncio.sleep(0.5)
+    if not await client.is_in_npc_range():
+        await client.teleport(-4442.06005859375, 1001.5532836914062)
+    await client.send_key(Keycode.X, 0.1)
+    await asyncio.sleep(0.7)
+    # Buy potions
+    for i in potion_ui_buy:
+        await client.mouse_handler.click_window_with_name(i)
+        await asyncio.sleep(0.1)
+    # Return
+    await client.send_key(Keycode.PAGE_UP, 0.1)
+    await client.wait_for_zone_change()
+    await client.send_key(Keycode.PAGE_DOWN, 0.1)
+
+
+async def safe_tp_to_mana(client):
+  try:
+    await client.tp_to_closest_mana_wisp()
+  except MemoryReadError:
+    await safe_tp_to_mana(client)
+
+
+async def safe_tp_to_health(client):
+  try:
+    await client.tp_to_closest_health_wisp()
+  except MemoryReadError:
+    await safe_tp_to_health(client)
+
+
+async def collect_wisps(client):
+    # Head to home world gate
+    await asyncio.sleep(0.5)
+    await client.send_key(Keycode.HOME, 0.1)
+    await client.wait_for_zone_change()
+    while not await client.is_in_npc_range():
+        await client.send_key(Keycode.S, 0.1)
+    await client.send_key(Keycode.X, 0.1)
+    await asyncio.sleep(0.5)
+    # Go to Mirage
+    for i in range(3):
+        await client.mouse_handler.click_window_with_name('rightButton')
+    await asyncio.sleep(0.1)
+    await client.mouse_handler.click_window_with_name('wbtnMirage')
+    await asyncio.sleep(0.1)
+    await client.mouse_handler.click_window_with_name('teleportButton')
+    await client.wait_for_zone_change()
+    # Collecting wisps
+    while await client.stats.current_hitpoints() < await client.stats.max_hitpoints():
+        await safe_tp_to_health(client)
+        await asyncio.sleep(0.4)
+    while await client.stats.current_mana() < await client.stats.max_mana():
+        await safe_tp_to_mana(client)
+        await asyncio.sleep(0.4)
+    # Return
+    await client.send_key(Keycode.PAGE_UP, 0.2)
+    await client.wait_for_zone_change()
+    await client.send_key(Keycode.PAGE_DOWN, 0.2)
+
+
+async def decide_heal(client):
+    if await client.stats.potion_charge() < 2.0:
+        print(f'[{client.title}] Needs potions, checking gold count')
+        print()
+        if await client.stats.current_gold() >= 25000:
+            print(f"[{client.title}] Enough gold, buying potions")
+            print()
+            await auto_buy_potions(client)
+        else:
+            print(f"[{client.title}] Low gold, collecting wisps")
+            print()
+            await collect_wisps(client)
+
+
+async def go_through_dialog(client):
+    while not await client.is_in_dialog():
+        await asyncio.sleep(0.1)
+    while await client.is_in_dialog():
+        await client.send_key(Keycode.SPACEBAR, 0.1)
